ecd533f64bf813c49a89f3d0a49257926b29b4bc
DefaltZ <priyongshupaul@gmail.com>
2025-03-12 17:57:43 +0530

    added tresjs integration (it sucks for now)

---
+++ src/components/SpinningCube.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { TresCanvas } from '@tresjs/core'
+import { useRenderLoop } from '@tresjs/core'
+import { ref } from 'vue'
+
+const cubeRotation = ref({ x: 0, y: 0 })
+
+useRenderLoop(() => {
+  cubeRotation.value.x += 0.01
+  cubeRotation.value.y += 0.01
+})
+</script>
+
+<template>
+  <TresCanvas window-size>
+    <TresAmbientLight :intensity="1" />
+    <TresDirectionalLight :position="[2, 2, 2]" :intensity="1" />
+    <TresMesh :rotation="[cubeRotation.x, cubeRotation.y, 0]">
+      <TresBoxGeometry :args="[1, 1, 1]" />
+      <TresMeshStandardMaterial color="#4169e1" />
+    </TresMesh>
+  </TresCanvas>
+</template>
+
+<style scoped>
+canvas {
+  width: 100% !important;
+  height: 300px !important;
+  background: transparent;
+}
+</style> 
